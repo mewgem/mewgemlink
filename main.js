853a3b66267149f7250df1fe26b909f12d01cb63
EmeraldMike <nekaoumike@gmail.com>
2023-05-16 18:16:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const ws = require('ws');
 const client = new ws('ws://localhost:4040');
-
-class app {
-    constructor (name,token) {
-        this.name = name;
+class mgapp {
+constructor (id,token) {
+        this.id = id;
         this.token = token;
     }
     connect = async () => {
@@ -14,7 +13,7 @@ class app {
                 channel: 'auth',
                 action: 'login',
                 auth:{
-                name: this.name,
+                id: this.id,
                 token: this.token
                 }
             }));
@@ -23,14 +22,53 @@ class app {
         let msg = JSON.parse(data);
         if(msg.channel == 'auth') {
             if(msg.status == 200) {
-                return resolve("Authed as "+this.name+"!")
+                this.name = msg.name;
+                return resolve("Authed as "+msg.name+"!")
             } else {
                 reject("Auth failed!")
             }
         }
+        if(msg.channel == 'error') {
+            reject(msg.error)
+        }
         })
     })
 }
+    createcall = async (user,perms,redirect) => {
+        return new Promise((resolve, reject) => {
+            if(!redirect) redirect = 'none';
+            client.send(JSON.stringify({
+                client: 'app',
+                channel: 'tpa',
+                action: 'createcall',
+                user: user,
+                perms: perms,
+                redirect: redirect,
+                auth:{
+                    id: this.id,
+                    token: this.token
+                    }
+            }));
+            client.on('message', (data) => {
+                let msg = JSON.parse(data);
+                if(msg.channel == 'tpa') {
+                    if(msg.status == 200) {
+                        return resolve(msg.url)
+                    } else {
+                        reject(msg.error)
+                    }
+                }
+                if(msg.channel == 'error') {
+                    reject(msg.error)
+                }
+            })
+        })
+    }
+    permTypes = {
+        VIEW_PROFILE: 'view_profile',
+        VIEW_FRIENDS: 'view_friends',
+        VIEW_GROUPS: 'view_groups',
+        VIEW_MESSAGES: 'view_messages',
+    }
 }
-
-module.exports = app;
+module.exports = mgapp;
